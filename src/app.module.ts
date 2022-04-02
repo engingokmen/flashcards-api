277f417b9a39d1a459b08e5f87399ae5ab1d1239
ENGİN GÖKMEN <engin.gokmen@ENGIN-MacBook-Pro.local>
2022-04-02 18:44:00 +0300

    env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,23 +7,27 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
-
-console.log('db');
-console.log('ABC');
-console.log('process.env.P_PASS', process.env.P_PASS);
-
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'postgres',
-      password: process.env.P_PASS,
-      database: 'postgres',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const DB_NAME = config.get<string>('DB_NAME');
+        return {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: 5432,
+          username: DB_NAME,
+          password: config.get<string>('P_PASS'),
+          database: DB_NAME,
+          entities: [User, Report],
+          synchronize: true,
+        };
+      },
     }),
     UsersModule,
     ReportsModule,
